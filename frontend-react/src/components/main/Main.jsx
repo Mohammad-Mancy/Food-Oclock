749bb314545e4d142e5b62dd18803b14c0044241
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-13 20:35:14 +0300

    Adding top navbar component

--- frontend-react/src/components/main/Main.jsx
+++ frontend-react/src/components/main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import TopNavBar from '../navbar/TopNavBar'
 
 const Main = () => {
   return (
     <div className="main-wrapper">
+        <TopNavBar/>
         main
     </div>
   )
---
+++ frontend-react/src/components/navbar/TopNavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../../assets/logo/logo-navbar.png'
+import { Link } from 'react-router-dom'
+import { FaUserAlt } from 'react-icons/fa';
+
+const TopNavBar = () => {
+  return (
+    <div className="top-navbar">
+        <Link to="/"><img src={logo}/></Link>
+        <div className="search">
+            <input type="text" className='search-input' placeholder='Search for a restaurant'/>
+        </div>
+
+            <div className="account-login">
+                <Link to="login" className='account-link'>
+                    <FaUserAlt/>
+                    <span> Login</span>
+                </Link>
+            </div>
+
+    </div>
+  )
+}
+
+export default TopNavBar

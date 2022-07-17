13173461665e1b2b472332d2880998cc5c94c4ac
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-17 14:59:18 +0300

    Adding add restaurant button on ManageRestaurant component

--- frontend-react/src/components/admin/ManageRestaurant.jsx
+++ frontend-react/src/components/admin/ManageRestaurant.jsx
@@ -8,6 +8,11 @@ const ManageRestaurant = () => {
     <div className="manage-restaurant-container">
       <AdminTopNavBar />
       <AdminMiddleNavBar />
+      <div className="adding-restaurant">
+        <span>Adding New Restuarant :</span>
+        <button>Add Restaurant</button>
+      </div>
+      <hr className="manage-restaurant-devider" />
       
       {/* Manage Restaurants Header */}
       <div className="manage-restaurant-header">

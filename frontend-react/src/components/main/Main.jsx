d7f74dd42b1a8578ffced02a06ab9409bf4fc62d
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-18 22:02:48 +0300

    Adding get all restaurant fetch API

--- frontend-react/src/components/main/Main.jsx
+++ frontend-react/src/components/main/Main.jsx
@@ -7,7 +7,31 @@ import { reactLocalStorage } from 'reactjs-localstorage'
 const Main = () => {
 
   const token_key = reactLocalStorage.get('token_key');
-  
+  const [restaurants,setRestaurants] = useState();
+  let handleRestaurant = async (e) => {
+    try{
+      let res = await fetch('http://127.0.0.1:8000/api/v1/auth/restaurant/get-restaurants',{
+        method: 'GET',
+        headers:{
+          'Content-Type' : 'application/json'
+        }
+      })
+      const data = await res.json();
+      if (res.status === 200 ) {
+        setRestaurants(data.restaurants)
+      }  
+    }catch(error){
+      console.error(error)
+    }
+    
+  };
+
+  useEffect ( () => {
+    handleRestaurant();
+  },[]);
+
+  console.log(restaurants);
+
   return (
     <div className="main-wrapper">
       {token_key !== undefined?

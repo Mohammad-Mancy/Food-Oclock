6263f73d26756ad13c7c8cb7c1c312ca599af0ff
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-21 13:26:07 +0300

    Appling search for a restaurant by collection

--- frontend-react/src/components/main/RestaurantByCollection.jsx
+++ frontend-react/src/components/main/RestaurantByCollection.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useRef} from 'react'
 import TopNavBar from '../navbar/TopNavBar'
 import RestaurantCard from './restaurants/RestaurantCard'
 import { reactLocalStorage } from 'reactjs-localstorage'
@@ -8,7 +8,9 @@ const RestaurantByCollection = () => {
 
     const token_key = reactLocalStorage.get('token_key');
     const [restaurants,setRestaurants] = useState([]);
+    const [filter, setFilter] = useState([]);
     const location = useLocation();
+    const filter_input = useRef();
 
     console.log(location.state)
 
@@ -23,6 +25,7 @@ const RestaurantByCollection = () => {
         const data = await res.json();
         if (res.status === 200 ) {
           setRestaurants(data.restaurants)
+          setFilter(data.restaurants)
         }  
       }catch(error){
         console.error(error)
@@ -35,15 +38,36 @@ const RestaurantByCollection = () => {
     },[]);
   
     console.log(restaurants);
+
+    // _______________ Search __________________________
+
+const filter_restaurants = () => {
+  var temp =[];
+  setFilter([]);
+  restaurants.forEach(restaurant => {
+    if(
+      restaurant.name.toLowerCase().includes(filter_input.current.value.toLowerCase())
+      ){
+          if(restaurant in temp){
+          }else{
+              temp[temp.length] = restaurant;
+              setFilter(temp);
+          }
+      }
+  });
+}
+
+// _________________________________________________
+
 return (
     <div className="restaurantByCollection-wrapper">
         {token_key !== undefined?
-      <TopNavBar status={'logout'}/>
+      <TopNavBar myRef={filter_input} onInput={() =>{filter_restaurants()}} status={'logout'}/>
       :
-      <TopNavBar status={true}/>}
+      <TopNavBar myRef={filter_input} onInput={() =>{filter_restaurants()}} status={true}/>}
       <div className="collection-name-header"><h1>{location.state.name}</h1></div>
             <div className="content-wrapper">
-          {restaurants.map(({id,name,rate,image,location_name,description})=>(
+          {filter.map(({id,name,rate,image,location_name,description})=>(
             <RestaurantCard 
             key={id}
             id={id}

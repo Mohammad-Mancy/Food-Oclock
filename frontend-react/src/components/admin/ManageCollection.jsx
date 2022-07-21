fc731c50c028891ef4bcf9d5559ec7c97fb9d5c1
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-21 17:25:53 +0300

    Adding get all collections fetch API (admin panel)

--- frontend-react/src/components/admin/ManageCollection.jsx
+++ frontend-react/src/components/admin/ManageCollection.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import AdminMiddleNavBar from '../navbar/AdminMiddleNavBar'
 import AdminTopNavBar from '../navbar/AdminTopNavBar'
 import AdminCollectionCard from './restaurant/AdminCollectionCard'
+import { reactLocalStorage } from 'reactjs-localstorage'
 
 const ManageCollection = () => {
+  
+  const user_type = reactLocalStorage.getObject('user').type;
+  const token_key = reactLocalStorage.get('token_key');
+  const [collections,setCollections] = useState([]);
+
+  let handleCollections = async (e) => {
+    try{
+      let res = await fetch('http://127.0.0.1:8000/api/v1/auth/restaurant/get-collections',{
+        method:'GET',
+        headers:{
+          'Content-Type' : 'application/json'}
+      })
+      const data = await res.json();
+      if(res.status === 200) {
+        setCollections(data.collections)
+      }
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect ( () => {
+    handleCollections();
+  },[]);
+
   return (
     <div className="manage-collection-container">
       <AdminTopNavBar />
@@ -22,13 +48,12 @@ const ManageCollection = () => {
       <hr className="manage-collection-devider" />
       {/* _________________________ */}
         
-      <AdminCollectionCard />
-      <AdminCollectionCard />
-      <AdminCollectionCard />
-      <AdminCollectionCard />
-      <AdminCollectionCard />
-      <AdminCollectionCard />
-      <AdminCollectionCard />
+      {collections.map(({id,name}) => (
+        <AdminCollectionCard 
+        key={id}
+        name={name}
+        />
+        ))}
     </div>
   )
 }
--- frontend-react/src/components/admin/restaurant/AdminCollectionCard.jsx
+++ frontend-react/src/components/admin/restaurant/AdminCollectionCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { RiDeleteBin5Line } from 'react-icons/ri'
 import { FaEdit } from 'react-icons/fa'
 
-const AdminCollectionCard = () => {
+const AdminCollectionCard = ({name}) => {
   return (
     <>
     <div className="collection-card-container">
-        <span>Collection Name</span>
+        <span>{name}</span>
         <span>
             <button className='collection-delete-btn'><RiDeleteBin5Line/></button>
             <button className='collection-edit-btn'><FaEdit/></button>

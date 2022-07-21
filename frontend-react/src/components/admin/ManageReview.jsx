9f07c18345111e0a00b57e2e33ad16ae9b132b82
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-21 16:41:11 +0300

    Adding get on progress fetch API

--- frontend-react/src/components/admin/ManageReview.jsx
+++ frontend-react/src/components/admin/ManageReview.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import AdminMiddleNavBar from '../navbar/AdminMiddleNavBar'
 import AdminTopNavBar from '../navbar/AdminTopNavBar'
 import AdminReviewCard from './restaurant/AdminReviewCard'
+import { reactLocalStorage } from 'reactjs-localstorage'
 
 const ManageReview = () => {
+
+  const user_type = reactLocalStorage.getObject('user').type;
+  const token_key = reactLocalStorage.get('token_key');
+  const [reviews,setReviews] = useState([]);
+
+  let handleReviews = async (e) => {
+    try{
+      let res = await fetch('http://127.0.0.1:8000/api/v1/auth/admin/all-on-progress-reviews',{
+        method:'POST',
+        headers:{
+          'Content-Type' : 'application/json',
+          'Authorization': `Bearer ${token_key}`},
+        body:JSON.stringify({
+          type:user_type
+      })
+      })
+      const data = await res.json();
+      if(res.status === 200) {
+        setReviews(data.reviews)
+      }
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect ( () => {
+    handleReviews();
+  },[]);
+
   return (
     <div className="manage-review-container">
         <AdminTopNavBar />
@@ -20,13 +50,16 @@ const ManageReview = () => {
         <hr className="manage-restaurant-devider" />
         {/* ___________________ */}
         
-        <AdminReviewCard />
-        <AdminReviewCard />
-        <AdminReviewCard />
-        <AdminReviewCard />
-        <AdminReviewCard />
-        <AdminReviewCard />
-        <AdminReviewCard />
+        {reviews.map(({id,user_name,restaurant_name,rate,description}) => (
+        <AdminReviewCard 
+        key={id}
+        user_name={user_name}
+        restaurant_name={restaurant_name}
+        rate={rate}
+        description={description}
+        />
+        ))}
+
 
     </div>
   )
--- frontend-react/src/components/admin/restaurant/AdminReviewCard.jsx
+++ frontend-react/src/components/admin/restaurant/AdminReviewCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { MdDone, MdClose } from 'react-icons/md'
 
-const AdminReviewCard = () => {
+const AdminReviewCard = ({id,user_name,restaurant_name,rate,description}) => {
   return (
     <>
     <div className="admin-review-card">
-        <span>Username</span>
-        <span>Restaurant Name</span>
-        <span>Rate</span>
-        <span>Description</span>
+        <span>{user_name}</span>
+        <span>{restaurant_name}</span>
+        <span>{rate}</span>
+        <span>{description}</span>
         <span>
             <button className='review-approve-btn'><MdDone/></button>
             <button className='review-disapprove-btn'><MdClose/></button>

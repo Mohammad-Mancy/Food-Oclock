687b0a028286033f718073d566fe14556c20602d
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-21 16:58:39 +0300

    Adding get all users fetch API

--- frontend-react/src/components/admin/ManageUser.jsx
+++ frontend-react/src/components/admin/ManageUser.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import AdminMiddleNavBar from '../navbar/AdminMiddleNavBar'
 import AdminTopNavBar from '../navbar/AdminTopNavBar'
 import UserCard from './user/UserCard'
+import { reactLocalStorage } from 'reactjs-localstorage'
 
 const ManageUser = () => {
+
+  const user_type = reactLocalStorage.getObject('user').type;
+  const token_key = reactLocalStorage.get('token_key');
+  const [users,setUsers] = useState([]);
+
+  let handleUsers = async (e) => {
+    try{
+      let res = await fetch('http://127.0.0.1:8000/api/v1/auth/admin/get-users',{
+        method:'POST',
+        headers:{
+          'Content-Type' : 'application/json',
+          'Authorization': `Bearer ${token_key}`},
+        body:JSON.stringify({
+          type:user_type
+      })
+      })
+      const data = await res.json();
+      if(res.status === 200) {
+        setUsers(data.users)
+      }
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect ( () => {
+    handleUsers();
+  },[]);
+
   return (
     <div className="manage-user">
         <AdminTopNavBar />
@@ -18,12 +48,14 @@ const ManageUser = () => {
           </div>
           <hr className="user-card-devider" />
         {/* _____________ */}
-
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        {users.map(({id,name,email,phone_number}) => (
+        <UserCard 
+        key={id}
+        name={name}
+        email={email}
+        phone_number={phone_number}
+        />
+        ))}
     </div>
   )
 }
--- frontend-react/src/components/admin/user/UserCard.jsx
+++ frontend-react/src/components/admin/user/UserCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {RiDeleteBin5Line} from 'react-icons/ri'
 
-const UserCard = () => {
+const UserCard = ({name,email,phone_number}) => {
   return (
     <>
     <div className="user-card">
-        <span>username</span>
-        <span>email</span>
-        <span>phone number</span>
+        <span>{name}</span>
+        <span>{email}</span>
+        <span>{phone_number}</span>
         <span>
             <button><RiDeleteBin5Line/></button>
         </span>

07a03870c237ca8f96577d9cd423185724920c74
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-07-30 14:21:22 +0300

    Initializing validation form for reservation

--- frontend-react/src/components/main/restaurants/reserve/ReservationCard.jsx
+++ frontend-react/src/components/main/restaurants/reserve/ReservationCard.jsx
@@ -7,14 +7,24 @@ import Row from 'react-bootstrap/esm/Row'
 import RangeSlider from 'react-bootstrap/FormRange'
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 const ReservationCard = () => {
 
+    const [show, setShow] = useState(false);
     const [fields,setFields] = useState(true)
-    const navigation = useNavigate();
+    const [fieldsVal,setFieldsVal] = useState(true)
     const [startDate, setStartDate] = useState(null);
     const [guestRange, setGuestRange] = useState(1);
-    const [note, setNote] = useState();
+    const [note, setNote] = useState('empty');
+    const [name,setName] = useState();
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const [phone_number,setPhone_number] = useState();
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     function addDays(date, days) {
         var result = new Date(date);
         result.setDate(result.getDate() + days);
@@ -29,12 +39,23 @@ const ReservationCard = () => {
         }
         setFields(true)
         e.preventDefault()
-        console.log('test')
+        handleShow()
     }
+
+    const handleReserveSubmit = (e) => {
+
+        if (!name || !email || !password || !phone_number)
+        {
+            setFieldsVal(false)
+            return
+        }
+        console.log(name + " " + email + " " + password +" " + phone_number + " " + startDate + " " + guestRange + " " + note)
+    }
+
     return (
         <div className='reservation-card'>
             <div className='calendar-title'>
-                <span className='required-date'>Check-in</span>
+                <span className='required'>Check-in</span>
                     <DatePicker
                         selected={startDate}
                         onChange={(date) => setStartDate(date)}
@@ -48,7 +69,7 @@ const ReservationCard = () => {
             </div>
 
             <div className='guest-range-slider'>
-                <span className='required-date'>How Many Guest's</span>
+                <span className='required'>How Many Guest's</span>
                 <Form style={{width:'80%'}}>
                     <Form.Group as={Row}>
                         <Col xs="9">
@@ -87,7 +108,85 @@ const ReservationCard = () => {
             {!fields ? 
                 <div className='validate-notification'>please fill all required field</div> 
             : <></>}
-            <Button variant="primary" className='reserve-btn' onClick={ (e) => { handleReserve(e) }}>Reserve</Button>
+            <Button 
+            variant="primary" 
+            className='reserve-btn' 
+            onClick={ 
+                (e) => { 
+                    handleReserve(e) 
+                    }}>Reserve</Button>
+
+
+            {/*_____________ VALIDATION FORM ________________ */}
+            <>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Validation</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+
+                        <Form.Group className="mb-3 validate-pop-up" >
+                            <Form.Label className='required'>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="name"
+                                autoFocus
+                                onChange={(e) => {setName(e.target.value)}}
+                            />
+                            </Form.Group>
+
+
+                            <Form.Group className="mb-3 validate-pop-up" >
+                            <Form.Label className='required'>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                                onChange={(e) => {setEmail(e.target.value)}}
+                            />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3 validate-pop-up" >
+                            <Form.Label className='required'>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => {setPassword(e.target.value)}}
+                            />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3 validate-pop-up" >
+                            <Form.Label className='required'>Phone Number</Form.Label>
+                            <Form.Control
+                                type="Phone"
+                                placeholder="phone number"
+                                onChange={(e) => {setPhone_number(e.target.value)}}
+                            />
+                            </Form.Group>
+
+                        </Form>
+                        {!fieldsVal ? 
+                            <div className='validate-notification'>please fill all required field</div> 
+                        : <></>}
+                    </Modal.Body>
+
+
+                    <Modal.Footer>
+                        
+
+                        <Button variant="danger" onClick={handleClose}>
+                            Cancel
+                        </Button>
+
+                        <Button variant="success" onClick={handleReserveSubmit}>
+                            Reserve
+                        </Button>
+
+                    </Modal.Footer>
+                </Modal>
+            </>
+            {/* _____________________________________ */}
+
       </div>
 
     );
